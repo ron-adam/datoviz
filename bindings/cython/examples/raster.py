a145f0675b78d18ff5e832ef6d43809514bf06a2
olivier <olivier.winter@hotmail.fr>
2021-03-20 17:25:13 +0000

    add cython key bindings

--- bindings/cython/examples/raster.py
+++ bindings/cython/examples/raster.py
@@ -16,10 +16,10 @@ c = canvas(show_fps=True)
 panel = c.panel(controller='axes')
 visual = panel.visual('point')
 
-amps = np.load(ROOT / "data/misc/spikes.amps.npy")
-spike_clusters = np.load(ROOT / "data/misc/spikes.clusters.npy")
-spike_depths = np.load(ROOT / "data/misc/spikes.depths.npy")
-spike_times = np.load(ROOT / "data/misc/spikes.times.npy")
+amps = np.load(ROOT.joinpath("data", "misc", "spikes.amps.npy"))
+spike_clusters = np.load(ROOT.joinpath("data", "misc", "spikes.clusters.npy"))
+spike_depths = np.load(ROOT.joinpath("data", "misc", "spikes.depths.npy"))
+spike_times = np.load(ROOT.joinpath("data", "misc", "spikes.times.npy"))
 
 N = len(spike_times)
 print(f"{N} spikes")
@@ -29,5 +29,11 @@ color = colormap(spike_clusters.astype(np.float64), cmap='glasbey', alpha=.5)
 visual.data('pos', pos)
 visual.data('color', color)
 visual.data('ms', np.array([2.]))
+
+@c.connect
+def on_key_press(key, modifiers=()):
+    print(key, modifiers)
+    if key == 'a':
+        pass
 
 run()
